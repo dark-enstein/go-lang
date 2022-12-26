5a26df18896d12062b02e546508aa831d200bc7e
Ayobami Bamigboye <ayobamibamigboye@Ayobamis-MBP.lan>
2022-12-26 03:32:33 +0100

    add globular script for build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo -------------------- BUILDING CODEGEN --------------------
+cd codegen/ ; go build -v ./...
+echo
+echo -------------------- BUILDING EMAIL --------------------
+cd email/ ; go build -v ./...
+echo
+echo -------------------- BUILDING CODEGEN --------------------
+cd pype/ ; go build -v ./...
